14611bac414e9951feb35f3cc06925eb96532869
BrianFaviansa <brianfpd31@gmail.com>
2024-10-09 11:22:03 +0700

    [Latihan Membuat Image Classification dengan TensorFlow Lite] done

--- MyTensorClassification/app/build.gradle.kts
+++ MyTensorClassification/app/build.gradle.kts
@@ -37,8 +37,11 @@ android {
 }
 
 dependencies {
-    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
-    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite:2.9.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.0")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
     val cameraxVersion = "1.3.4"
 
     implementation("androidx.core:core-ktx:1.13.1")
@@ -48,7 +51,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.9.0")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
