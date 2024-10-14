27bdd40617514621f3ad39add10fcab4db9bc036
BrianFaviansa <brianfpd31@gmail.com>
2024-10-14 11:47:56 +0700

    feat: classifier helper & image picker from gallery

--- Asclepius/app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ Asclepius/app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -2,38 +2,115 @@ package com.dicoding.asclepius.view
 
 import android.Manifest
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.startActivity
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.dicoding.asclepius.helper.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
 
     private var currentImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        imageClassifierHelper = ImageClassifierHelper(
+            context = this@MainActivity,
+            classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    showToast(error)
+                }
+
+                override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                    runOnUiThread {
+                        binding.progressIndicator.visibility = View.GONE
+                        try {
+                            results?.let {
+                                val category = it[0].categories[0].label
+                                val confidence = it[0].categories[0].score
+
+                                currentImageUri?.let { uri ->
+                                    this@MainActivity.contentResolver.openInputStream(uri)
+                                        ?.use { inputStream ->
+                                            val bitmap = BitmapFactory.decodeStream(inputStream)
+                                            binding.previewImageView.setImageBitmap(bitmap)
+                                        }
+                                }
+
+
+                                val confidencePercentage = convertToPercent(confidence)
+                                moveToResult("Dikategorikan sebagai: $category, dengan tingkat kepastian (confidence): $confidencePercentage")
+                            }
+                        } catch (e: Exception) {
+                            onError(e.message.toString())
+                        }
+                    }
+                }
+            }
+        )
+
+        binding.galleryButton.setOnClickListener { startGallery() }
+        binding.analyzeButton.setOnClickListener { analyzeImage() }
     }
 
     private fun startGallery() {
-        // TODO: Mendapatkan gambar dari Gallery.
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
 
     private fun showImage() {
-        // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
     private fun analyzeImage() {
-        // TODO: Menganalisa gambar yang berhasil ditampilkan.
+        if (currentImageUri != null) {
+            binding.progressIndicator.visibility = View.VISIBLE
+            imageClassifierHelper.classifyStaticImage(currentImageUri!!)
+        }
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri.toString())
+        startActivity(intent)
     }
 
-    private fun moveToResult() {
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun moveToResult(prediction: String) {
         val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra(ResultActivity.EXTRA_IMAGE_URI, currentImageUri)
+        intent.putExtra(ResultActivity.EXTRA_PREDICTION, prediction)
         startActivity(intent)
+    }
+
+    fun convertToPercent(value: Float): String {
+        val percentage = value * 100
+        val roundedPercentage = kotlin.math.round(percentage).toInt()
+        return "$roundedPercentage%"
     }
 
     private fun showToast(message: String) {
--- Asclepius/app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ Asclepius/app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -1,7 +1,11 @@
 package com.dicoding.asclepius.view
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
@@ -10,10 +14,19 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
 
-        // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imageUri = intent.getParcelableExtra<Uri>(EXTRA_IMAGE_URI)
+        val prediction = intent.getStringExtra(EXTRA_PREDICTION)
+
+        binding.resultImage.setImageURI(imageUri)
+        binding.resultText.text = prediction
     }
 
-
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image"
+        const val EXTRA_PREDICTION = "extra_prediction"
+    }
 }
